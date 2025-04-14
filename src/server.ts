44708dfe0d2141fd39899547649642dd188c59a9
Erick Gabriel <erickgabrielalves@hotmail.com>
2025-04-14 16:03:46 -0300

    feat: deploy vercel

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { env } from "./env";
 
 app.listen({ port: env.PORT }).then(() => {
     console.log("HTTP Server Running at: http://localhost:3333");
-    console.log("API Documentation Running at: http://localhost:3333/docs")
+    console.log("API Documentation Running at: http://localhost:3333/docs");
 });
